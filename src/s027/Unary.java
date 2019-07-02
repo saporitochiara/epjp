9d485ceb1ba547d0047c750e09cc2ab6a9e93d4a
Manny <egalli64@gmail.com>
2019-07-02 11:15:08 +0200

    unary operators

---
+++ src/s027/Unary.java
@@ -0,0 +1,20 @@
+package s027;
+
+public class Unary {
+    public static void main(String[] args) {
+        int value = 1;
+
+        System.out.println(value); // 1
+
+        System.out.println(++value); // 2
+        System.out.println(--value); // 1
+
+        System.out.println(value++); // 1
+        System.out.println(value); // 2
+        System.out.println(value--); // 2
+        System.out.println(value); // 1
+
+        System.out.println(+value); // 1
+        System.out.println(-value); // -1
+    }
+}
