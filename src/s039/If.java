8b32bcfae86d3134f9649a10066d5e8fc7ddd02e
Manny <egalli64@gmail.com>
2019-07-02 22:20:52 +0200

    if - else if - else

---
+++ src/s039/If.java
@@ -0,0 +1,13 @@
+package s039;
+
+public class If {
+    public static void main(String[] args) {
+        boolean condition = true;
+        
+        if (condition) {
+            System.out.println("only if condition is true");
+        }
+
+        System.out.println("always printed");
+    }
+}
