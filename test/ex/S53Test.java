497eb5e535bb9b3a9a00435fe4ddf9fdd10bb3d6
Manny <egalli64@gmail.com>
2019-07-04 22:24:10 +0200

    naming cleanup

--- test/ex/S53Test.java
+++ test/ex/S53Test.java
@@ -29,28 +29,28 @@ class S53Test {
     }
 
     @Test
-    void testIsOddTrue() {
+    void isOddTrue() {
         boolean actual = S53.isOdd(19);
 
         assertThat(actual, is(true));
     }
 
     @Test
-    void testIsOddFalse() {
+    void isOddFalse() {
         boolean actual = S53.isOdd(-12);
 
         assertThat(actual, is(false));
     }
 
     @Test
-    void testAsWordZero() {
+    void asWordZero() {
         String actual = S53.asWord(0);
 
         assertThat(actual, is("zero"));
     }
 
     @Test
-    void testAsWordOther() {
+    void asWordOther() {
         String actual = S53.asWord(42);
 
         assertThat(actual, is("other"));
