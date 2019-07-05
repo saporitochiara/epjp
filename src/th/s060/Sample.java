7df8ce9275d77f85228649f71cd39b90fc3de069
Manny <egalli64@gmail.com>
2019-07-05 12:13:29 +0200

    variable initialization

---
+++ src/th/s060/Sample.java
@@ -0,0 +1,18 @@
+package th.s060;
+
+public class Sample {
+    private int i; // 0
+    private boolean flag; // false
+    private String t; // null
+
+    public void f() {
+        int i = 42;
+        String s = new String("Hello");
+
+        System.out.println(s + i);
+    }
+
+    public void g() {
+        System.out.println(t + ", " + i + ", " + flag);
+    }
+}
