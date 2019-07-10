cd688016669762632ef5fffbe2266a1f338a0426
Manny <egalli64@gmail.com>
2019-07-10 11:08:21 +0200

    ArrayList ctor example

---
+++ src/th/s102/ArrayListExample.java
@@ -0,0 +1,11 @@
+package th.s102;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<>(Arrays.asList(23, 12, 18, 5));
+        System.out.println(al);
+    }
+}
