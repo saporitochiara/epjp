330df65585c8dba346fff0ee2cfe540370cf32d6
Manny <egalli64@gmail.com>
2019-07-11 12:27:02 +0200

    Subqueries

---
+++ scripts/s50.sql
@@ -0,0 +1,15 @@
+select first_name, last_name
+from employees
+where employee_id = (
+	select manager_id from employees where last_name = 'Chen');
+
+select department_id, trunc(avg(salary))
+from employees
+group by department_id having avg(salary) < (
+    select max(avg(salary)) from employees group by department_id);
+
+select employee_id
+from (
+	select employee_id
+	from employees
+	where employee_id between 112 and 115);
