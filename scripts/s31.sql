a7194fb9429cfcb1bba65adc8af284ef4bed45b1
Manny <egalli64@gmail.com>
2019-07-11 11:29:51 +0200

    self join

---
+++ scripts/s31.sql
@@ -0,0 +1,7 @@
+select e.last_name as employee, m.last_name as manager
+from employees e join employees m
+on (e.manager_id = m.employee_id);
+
+select e.last_name as employee, m.last_name as manager
+from employees e, employees m
+where e.manager_id = m.employee_id;
