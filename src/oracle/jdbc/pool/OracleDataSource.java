2eee5fe91e72bd269708116a03b250de77ec002f
Manny <egalli64@gmail.com>
2019-07-19 09:09:37 +0200

    Using temporary mock implementation for oracle jdbc

---
+++ src/oracle/jdbc/pool/OracleDataSource.java
@@ -0,0 +1,23 @@
+/**
+ * Mock implementation
+ */
+package oracle.jdbc.pool;
+
+import java.sql.Connection;
+
+public class OracleDataSource {
+
+	public void setURL(String url) {
+	}
+
+	public void setUser(String user) {
+	}
+
+	public void setPassword(String password) {
+	}
+
+	public Connection getConnection() {
+		return null;
+	}
+
+}
