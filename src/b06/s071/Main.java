a0f1ed758f213a315d1fe65dc26f539ef792f052
Emanuele Galli <egalli64@gmail.com>
2019-08-01 11:55:11 +0200

    this vs super example

---
+++ src/b06/s071/Main.java
@@ -0,0 +1,11 @@
+package b06.s071;
+
+public class Main {
+	public static void main(String[] args) {
+		Poodle poodle = new Poodle("Jim");
+		Poodle p2 = new Poodle();
+
+		System.out.println(poodle);
+		System.out.println(p2);
+	}
+}
