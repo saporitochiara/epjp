0f145bf8a3bcf5c05fff67aa2890a5b173e75708
Manny <egalli64@gmail.com>
2019-08-03 11:07:15 +0200

    flatter hierarchy for block 3

---
+++ src/b03/S37ArrayShortcut.java
@@ -0,0 +1,11 @@
+package b03;
+
+public class S37ArrayShortcut {
+    public static void main(String[] args) {
+        int[] array = { 1, 4, 3 };
+
+        // array[array.length] = 21; // exception
+        
+        System.out.println(array.length); // 3
+    }
+}
