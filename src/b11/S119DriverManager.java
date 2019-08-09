998f578a580ff75312d3f8bf020eab2011443d08
egalli64 <egalli64@gmail.com>
2019-08-09 15:14:36 +0200

    b11 review, more stress on DriverManager

--- src/b11/S119DriverManager.java
+++ src/b11/S119DriverManager.java
@@ -1,24 +1,17 @@
 package b11;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import oracle.jdbc.pool.OracleDataSource;
-
-public class S115 {
+public class S119DriverManager {
     private static final String URL = "jdbc:oracle:thin:@localhost:1521/xepdb1";
     private static final String USER = "me";
     private static final String PASSWORD = "password";
 
     public static void main(String[] args) {
         try {
-            OracleDataSource ods = new OracleDataSource();
-            
-            ods.setURL(URL);
-            ods.setUser(USER);
-            ods.setPassword(PASSWORD);
-            
-            Connection conn = ods.getConnection();
+            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
             System.out.println("Connected as " + conn.getSchema());
         } catch (SQLException e) {
             e.printStackTrace();
