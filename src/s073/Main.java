50d5aeb8bb3323b094196ba206e7c8c85419e0bf
Administrator <Administrator@192.168.20.58>
2019-09-23 13:20:46 +0200

    inizio seconda settimana

--- src/s073/Main.java
+++ src/s073/Main.java
@@ -1,22 +1,24 @@
 package s073;
 
 public class Main {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 //		Cat cat = (Cat) new Dog();
 
-        Pet pet = new Dog("Bob");
+		Pet pet = new Dog("Bob");
 
-        // risky
-        Dog dog = (Dog) pet;
-        dog.bark();
+		// risky
+		Dog dog = (Dog) pet;
+		dog.bark();
 
-        // next line leads to a ClassCastException
-        Cat cat = (Cat) pet;
-        System.out.println(System.identityHashCode(cat));
+		// next line leads to a ClassCastException
+//        Cat cat = (Cat) pet;
+//        System.out.println(System.identityHashCode(cat));
 
-        if (pet instanceof Cat) {
-            Cat tom = (Cat) pet;
-            tom.meow();
-        }
-    }
+		if (pet instanceof Cat) {
+			Cat tom = (Cat) pet;
+			tom.meow();
+		} else {
+			System.out.println("This pet is not a cat!");
+		}
+	}
 }
